89fc84c2d00039b799e1a6ca1801091588e1c671
userlab-cesarsalazar <cesar.augs@mgail.com>
2021-06-18 21:54:53 -0500

    - get manifest - get manifes detail

--- Api/Manifest/handler.js
+++ Api/Manifest/handler.js
@@ -2,9 +2,7 @@
 const mysql = require('mysql2/promise')
 const isOffline = process.env['IS_OFFLINE']
 const { dbConfig } = require(`${isOffline ? '../..' : '.'}/commons/dbConfig`)
-const { response, fileResponse, getBody, escapeFields } = require(`${
-    isOffline ? '../..' : '.'
-}/commons/utils`)
+const { response, fileResponse, getBody, escapeFields } = require(`${isOffline ? '../..' : '.'}/commons/utils`)
 const storage = require('./manifestStorage')
 
 const AWS = require('aws-sdk')
@@ -12,200 +10,217 @@ const Excel = require('exceljs')
 AWS.config.update({ region: 'us-east-1' })
 
 module.exports.readManifest = async (event, context) => {
-    const connection = await mysql.createConnection(dbConfig)
-    try {
-        const [manifests] = await connection.execute(storage.readManifest())
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const status = event.queryStringParameters && event.queryStringParameters.status ? event.queryStringParameters.status : undefined
 
-        return response(200, manifests, connection)
-    } catch (error) {
-        const message = error.message ? error.message : error
+    const [manifests] = await connection.execute(storage.readManifest(status))
 
-        return await response(400, { error: message }, connection)
-    }
+    return response(200, manifests, connection)
+  } catch (error) {
+    const message = error.message ? error.message : error
+
+    return await response(400, { error: message }, connection)
+  }
 }
 
 module.exports.createManifest = async (event, context) => {
-    const connection = await mysql.createConnection(dbConfig)
-    try {
-        const requiredFields = ['description']
-        const body = escapeFields(getBody(event))
-        const errorFields = requiredFields.filter(k => !body[k])
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const requiredFields = ['description']
+    const body = escapeFields(getBody(event))
+    const errorFields = requiredFields.filter(k => !body[k])
 
-        if (errorFields.length > 0 || !body) {
-            throw new Error(`The fields ${errorFields.join(', ')} are required`)
-        }
-
-        const [manifest] = await connection.execute(storage.createManifest(body))
-
-        return response(200, manifest, connection)
-    } catch (error) {
-        const message = error.message ? error.message : error
-
-        return await response(400, { error: message }, connection)
+    if (errorFields.length > 0 || !body) {
+      throw new Error(`The fields ${errorFields.join(', ')} are required`)
     }
+
+    const [manifest] = await connection.execute(storage.createManifest(body))
+
+    return response(200, manifest, connection)
+  } catch (error) {
+    const message = error.message ? error.message : error
+
+    return await response(400, { error: message }, connection)
+  }
 }
 
 module.exports.updateManifest = async (event, context) => {
-    const connection = await mysql.createConnection(dbConfig)
-    try {
-        const id =
-            event.pathParameters && event.pathParameters.id
-                ? JSON.parse(event.pathParameters.id)
-                : undefined
-        const requiredFields = ['description','status']
-        const body = escapeFields(getBody(event))
-        const errorFields = requiredFields.filter(k => !body[k])
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const id = event.pathParameters && event.pathParameters.id ? JSON.parse(event.pathParameters.id) : undefined
+    const requiredFields = ['description', 'status']
+    const body = escapeFields(getBody(event))
+    const errorFields = requiredFields.filter(k => !body[k])
 
-        if (errorFields.length > 0) {
-            throw new Error(`The fields ${errorFields.join(', ')} are required`)
-        }
-
-        const [manifest] = await connection.execute(
-            storage.updateManifest(body, id)
-        )
-
-        return response(200, manifest, connection)
-    } catch (error) {
-        const message = error.message ? error.message : error
-
-        return await response(400, { error: message }, connection)
+    if (errorFields.length > 0) {
+      throw new Error(`The fields ${errorFields.join(', ')} are required`)
     }
+
+    const [manifest] = await connection.execute(storage.updateManifest(body, id))
+
+    return response(200, manifest, connection)
+  } catch (error) {
+    const message = error.message ? error.message : error
+
+    return await response(400, { error: message }, connection)
+  }
 }
 
 module.exports.readMaxManifest = async (event, context) => {
-    const connection = await mysql.createConnection(dbConfig)
-    try {
-        const [manifest] = await connection.execute(storage.getMAXManifest())
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const [manifest] = await connection.execute(storage.getMAXManifest())
 
-        return response(200, manifest, connection)
-    } catch (error) {
-        const message = error.message ? error.message : error
+    return response(200, manifest, connection)
+  } catch (error) {
+    const message = error.message ? error.message : error
 
-        return await response(400, { error: message }, connection)
-    }
+    return await response(400, { error: message }, connection)
+  }
 }
 
-module.exports.readPackagesByManifest = async () => {
+module.exports.readPackagesByManifest = async event => {
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const manifest_id = event.pathParameters && event.pathParameters.id ? JSON.parse(event.pathParameters.id) : undefined
 
+    let [result] = await connection.execute(storage.getPackagesByManifestId(manifest_id))
+
+    return response(200, result, connection)
+  } catch (error) {
+    const message = error.message ? error.message : error
+
+    return await response(400, { error: message }, connection)
+  }
 }
 
-module.exports.exportManifest = async (event) => {
-    const connection = await mysql.createConnection(dbConfig)
-    try {
-        const manifest_id =
-            event.pathParameters && event.pathParameters.id
-                ? JSON.parse(event.pathParameters.id)
-                : undefined
+module.exports.exportManifest = async event => {
+  const connection = await mysql.createConnection(dbConfig)
+  try {
+    const manifest_id = event.pathParameters && event.pathParameters.id ? JSON.parse(event.pathParameters.id) : undefined
 
-        let [result] = await connection.execute(storage.getPackagesByManifestId(manifest_id))
-        let report;
-        let file;
+    let [result] = await connection.execute(storage.getPackagesByManifestId(manifest_id))
+    let report
+    let file
 
-        console.log('result ',result)
+    console.log('result ', result)
 
-        let manifestoHeaders = [
-            {name: 'Tracking', column: 'tracking', width: 33.50},
-            {name: 'WareHouse', column: 'warehouse', width: 9.83}, //Missing
-            {name: 'Proveedor', column: 'supplier_name', width: 15},
-            {name: 'Consignatario', column: 'client_name', width: 43.33},
-            {name: 'Piezas', column: 'Piezas', width: 6}, //Missing
-            {name: 'Peso Kg', column: 'weight', width: 6.33},
-            {name: 'Valor Flete', column: 'ValorFlete', width: 15}, //Missing
-            {name: 'Descripcion', column: 'description', width: 51.50},
-            {name: 'Valor Declarado', column: 'costo_producto', width: 16.17, numFmt: '"$"#,##0.00'}, //Missing
-        ];
+    let manifestoHeaders = [
+      { name: 'Tracking', column: 'tracking', width: 33.5 },
+      { name: 'WareHouse', column: 'warehouse', width: 9.83 }, //Missing
+      { name: 'Proveedor', column: 'supplier_name', width: 15 },
+      { name: 'Consignatario', column: 'client_name', width: 43.33 },
+      { name: 'Piezas', column: 'Piezas', width: 6 }, //Missing
+      { name: 'Peso Kg', column: 'weight', width: 6.33 },
+      { name: 'Valor Flete', column: 'ValorFlete', width: 15 }, //Missing
+      { name: 'Descripcion', column: 'description', width: 51.5 },
+      {
+        name: 'Valor Declarado',
+        column: 'costo_producto',
+        width: 16.17,
+        numFmt: '"$"#,##0.00',
+      }, //Missing
+    ]
 
-        let weightHeaders = [
-            {name: 'TULA', column: 'tula', width: 18},
-            {name: 'PESOS', column: 'pesos', width: 18}
-        ];
+    let weightHeaders = [
+      { name: 'TULA', column: 'tula', width: 18 },
+      { name: 'PESOS', column: 'pesos', width: 18 },
+    ]
 
-        report = await standardReport({
-            sheets: [
-                {
-                    name: `MANIFIESTO`,
-                    headers: manifestoHeaders,
-                    data: result,
-                },
-                {
-                    name: 'PESOS',
-                    headers: weightHeaders,
-                    data: []
-                }
-            ],
-        });
+    report = await standardReport({
+      sheets: [
+        {
+          name: `MANIFIESTO`,
+          headers: manifestoHeaders,
+          data: result,
+        },
+        {
+          name: 'PESOS',
+          headers: weightHeaders,
+          data: [],
+        },
+      ],
+    })
 
-        file = await report.xlsx.writeBuffer();
+    file = await report.xlsx.writeBuffer()
 
-        return await fileResponse(200, file.toString('base64'), connection,manifest_id)
+    return await fileResponse(200, file.toString('base64'), connection, manifest_id)
+  } catch (error) {
+    const message = error.message ? error.message : error
 
-
-    } catch (error) {
-        const message = error.message ? error.message : error
-
-        return await response(400, { error: message }, connection)
-    }
-
-
+    return await response(400, { error: message }, connection)
+  }
 }
 
 const standardReport = data =>
-    new Promise((resolve, reject) => {
-        let workbook = new Excel.Workbook();
-        workbook.creator = 'TraesTodo';
-        workbook.created = new Date();
+  new Promise((resolve, reject) => {
+    let workbook = new Excel.Workbook()
+    workbook.creator = 'TraesTodo'
+    workbook.created = new Date()
 
-        let auxCol = null;
+    let auxCol = null
 
-        if (data && data.sheets && data.sheets.length > 0) {
-            data.sheets.forEach((sheet, i) => {
-                let newSheet = workbook.addWorksheet(sheet.name, {
-                    headerFooter:{firstHeader: "Hello Exceljs", firstFooter: "Hello World"}
-                });
+    if (data && data.sheets && data.sheets.length > 0) {
+      data.sheets.forEach((sheet, i) => {
+        let newSheet = workbook.addWorksheet(sheet.name, {
+          headerFooter: {
+            firstHeader: 'Hello Exceljs',
+            firstFooter: 'Hello World',
+          },
+        })
 
-                newSheet.columns = sheet.headers.map(header => ({
-                    header: header.name,
-                    key: header.column,
-                    width: header.width || 15,
-                }));
+        newSheet.columns = sheet.headers.map(header => ({
+          header: header.name,
+          key: header.column,
+          width: header.width || 15,
+        }))
 
-                newSheet.getRow(1).font = { bold: true, color: { argb: 'ffffff' } };
+        newSheet.getRow(1).font = { bold: true, color: { argb: 'ffffff' } }
 
-                newSheet.getRow(1).alignment = { wrapText: true };
+        newSheet.getRow(1).alignment = { wrapText: true }
 
-                newSheet.getRow(1).fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: '053E81' } };
-
-                newSheet.addRows(sheet.data.map(d => d));
-
-                sheet.headers.forEach((h, c) => {
-                    if (h.formula) {
-                        auxCol = newSheet.getColumn(h.column);
-                        auxCol.eachCell((cell, i) => {
-                            if (i > 1) {
-                                cell.value = { formula: h.formula.replace(/#/g, i) };
-                            }
-                        });
-                    }
-
-                    if (h.numFmt) {
-                        newSheet.getColumn(h.column).numFmt = h.numFmt;
-                    }
-                });
-
-                if (sheet.totals && sheet.totals.length > 0) {
-                    sheet.totals.forEach(t => {
-                        newSheet.getCell(`${t}${sheet.data.length + 2}`).value = { formula: `SUM(${t}2:${t}${sheet.data.length + 1})` };
-                        newSheet.getCell(`${t}${sheet.data.length + 2}`).font = { bold: true };
-                    });
-                }
-                if (sheet.individualCells && sheet.individualCells.length > 0) {
-                    sheet.individualCells.forEach(cell => {
-                        newSheet.getCell(`${cell.name}`).value = cell.value ? cell.value : { formula: `${cell.formula}` };
-                        newSheet.getCell(`${cell.name}`).font = { bold: cell.bold };
-                    });
-                }
-            });
+        newSheet.getRow(1).fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: '053E81' },
         }
 
-        resolve(workbook);
-    });
+        newSheet.addRows(sheet.data.map(d => d))
+
+        sheet.headers.forEach((h, c) => {
+          if (h.formula) {
+            auxCol = newSheet.getColumn(h.column)
+            auxCol.eachCell((cell, i) => {
+              if (i > 1) {
+                cell.value = { formula: h.formula.replace(/#/g, i) }
+              }
+            })
+          }
+
+          if (h.numFmt) {
+            newSheet.getColumn(h.column).numFmt = h.numFmt
+          }
+        })
+
+        if (sheet.totals && sheet.totals.length > 0) {
+          sheet.totals.forEach(t => {
+            newSheet.getCell(`${t}${sheet.data.length + 2}`).value = {
+              formula: `SUM(${t}2:${t}${sheet.data.length + 1})`,
+            }
+            newSheet.getCell(`${t}${sheet.data.length + 2}`).font = {
+              bold: true,
+            }
+          })
+        }
+        if (sheet.individualCells && sheet.individualCells.length > 0) {
+          sheet.individualCells.forEach(cell => {
+            newSheet.getCell(`${cell.name}`).value = cell.value ? cell.value : { formula: `${cell.formula}` }
+            newSheet.getCell(`${cell.name}`).font = { bold: cell.bold }
+          })
+        }
+      })
+    }
+
+    resolve(workbook)
+  })
--- Api/Manifest/manifestStorage.js
+++ Api/Manifest/manifestStorage.js
@@ -2,27 +2,34 @@ const createManifest = data => ` INSERT INTO manifest
                   (description, status)
                   VALUES('${data.description.toUpperCase()}', 'OPEN');`
 
-const readManifest = () =>  ` SELECT *
-                  FROM manifest A
-                  WHERE status = 'OPEN'
-                  ORDER by A.manifest_id DESC`
+const readManifest = status => {
+  console.log(status)
+  const WHERE = status ? `status = '${status}'` : `1=1`
+  const query = ` SELECT *
+                    FROM manifest A
+                    WHERE ${WHERE}
+                    ORDER by A.manifest_id DESC LIMIT 25`
+  return query
+}
 
 const getMAXManifest = () => `SELECT MAX(manifest_id) as manifest_id from manifest`
 
 const updateManifest = (data, id) => `UPDATE manifest SET description='${data.description.toUpperCase()}', 
                                       status='${data.status}' WHERE manifest_id=${id};`
 
-const getPackagesByManifestId = (manifest_id) => `SELECT A.tracking, S.name as supplier_name, C.client_name, A.weight, A.description,
-                                                  A.client_id as warehouse, A.costo_producto 
-                                                  FROM paquetes A
-                                                  LEFT JOIN clientes C on A.client_id = C.client_id 
-                                                  LEFT JOIN suppliers S on A.supplier_id = S.id
-                                                  WHERE manifest_id = ${manifest_id}`
+const getPackagesByManifestId = manifest_id =>
+  `SELECT A.tracking, S.name as supplier_name, C.client_name, A.weight, A.description,
+    A.client_id as warehouse, A.costo_producto, A.cif, A.tasa, A.status, A.importe, A.guia, A.cif, A.dai,
+    A.master, A.poliza, A.manifest_id, A.total_iva, A.total_a_pagar, A.ing_date
+    FROM paquetes A
+    LEFT JOIN clientes C on A.client_id = C.client_id 
+    LEFT JOIN suppliers S on A.supplier_id = S.id
+    WHERE manifest_id = ${manifest_id}`
 
-module.exports =  {
-    createManifest,
-    readManifest,
-    updateManifest,
-    getMAXManifest,
-    getPackagesByManifestId
+module.exports = {
+  createManifest,
+  readManifest,
+  updateManifest,
+  getMAXManifest,
+  getPackagesByManifestId,
 }
