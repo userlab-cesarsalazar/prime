b537005ae8117b417aac483dc633854ca844a3e8
userlab-luisdeleon <luis.deleon@userlab.co>
2021-08-23 16:38:26 -0600

    Update handler.js
    modified SMS to be shorter

--- Api/Packages/handler.js
+++ Api/Packages/handler.js
@@ -426,17 +426,18 @@ module.exports.sendSMSTigo = async event => {
       switch (params.data.client_id.charAt(0)) {
         case 'P':
           SMS = params.warehouse ?
-          `NOW EXPRESS, hemos recibido en nuestras bodegas de MIAMI tu paquete con tracking ${params.data.tracking}.  Cualquier consulta contáctanos a 2376-4699 / 3237-0023.`
-          : `NOW EXPRESS, tu paquete esta listo para entrega en Guatemala! Tracking ${params.data.tracking}, Cliente: ${params.data.client_id} Total: ${params.data.total} Contáctanos: 2376-4699 / 3237-0023`
+          `NOW EXPRESS, hemos recibido en nuestras bodegas de MIAMI tu paquete: ${params.data.tracking}. Para consultas contáctanos a 2376-4699 / 3237-0023.`
+          : `NOW EXPRESS, Informa tu paquete: ${params.data.tracking}, Cliente: ${params.data.client_id}, Total: ${params.data.total} esta listo para entrega. Contáctanos a 2376-4699 / 3237-0023`
           break
         case 'T':
           SMS = params.warehouse ? 
-          `TRAESTODO, hemos recibido en nuestras bodegas de MIAMI tu paquete con tracking ${params.data.tracking}, Cualquier consulta contáctanos al 4154-4275`
-          :`TRAESTODO, tu paquete esta listo para entrega en Guatemala! Tracking ${params.data.tracking}, Cliente: ${params.data.client_id}, LBs: ${params.data.weight}. Contáctanos: 4154-4275`
+          `TRAESTODO, hemos recibido en nuestras bodegas de MIAMI tu paquete: ${params.data.tracking}, Para consultas contáctanos a 4154-4275`
+          :`TRAESTODO, Informa tu paquete: ${params.data.tracking}, Cliente: ${params.data.client_id}, LBs: ${params.data.weight}. esta listo para entrega. Contáctanos a 4154-4275`
           break
         default:
-          SMS = params.warehouse ? `Rapidito Express, hemos recibido en nuestras bodegas de MIAMI tu paquete con tracking ${params.data.tracking}, Cualquier consulta contáctanos al 5803-2545 o email: Info@rapiditoexpress.com`
-          :`Rapidito Express, tu paquete esta listo para entrega en Guatemala! Tracking ${params.data.tracking}, Cliente: ${params.data.client_id}, LBs: ${params.data.weight}. Contáctanos: 5803-2545 o email: Info@rapiditoexpress.com`
+          SMS = params.warehouse ? 
+          `Rapidito Express, hemos recibido en nuestras bodegas de MIAMI tu paquete: ${params.data.tracking}, Para consultas contáctanos a 5803-2545`
+          :`Rapidito Express, Informa tu paquete: ${params.data.tracking}, Cliente: ${params.data.client_id}, LBs: ${params.data.weight}. esta listo para entrega. Contáctanos a 5803-2545`
       }
     
     const phone = `502${params.profile[0].phone}`
