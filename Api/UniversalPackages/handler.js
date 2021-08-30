d8379a7652388a8ebc17e7fb72222858d5a42dac
userlab-cesarsalazar <cesar.augs@mgail.com>
2021-08-30 13:26:15 -0500

    - api24 integration

--- Api/UniversalPackages/handler.js
+++ Api/UniversalPackages/handler.js
@@ -1,6 +1,7 @@
 'use strict'
 const mysql = require('mysql2/promise')
 const moment = require('moment-timezone')
+const request = require('request')
 const isOffline = process.env['IS_OFFLINE']
 const { dbConfig } = require(`${isOffline ? '../..' : '.'}/commons/dbConfig`)
 let { response } = require(`${isOffline ? '../..' : '.'}/commons/utils`)
@@ -15,12 +16,36 @@ AWS.config.update({ region: 'us-east-1' })
 
 module.exports.read = async (event, context) => {
   try {
-    const params = event.queryStringParameters;
+    let params = {}
+    
+    if(event.pathParameters.tracking)
+      params.trackingNumber = event.pathParameters.tracking
+    console.log(process.env['TOKEN_24API']);
+    console.log(process.env['SHIP_API']);
+    let api24 = await new Promise((resolve, reject) => {
+      request.post({
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': 'Bearer apik_gwIJFvWHJCCIWYG8FR4g4RcsU8FkwM'
+        },
+        url: 'https://api.ship24.com/public/v1/tracking/search',
+        body: params,
+        json:true
+      }, function(error, response, body){
+        console.log(body);
+        resolve(body)
+      });
+    })
     
     const connection = await mysql.createConnection(dbConfig)
     const [packages] = await connection.execute(storage.get(params))
+    
+    let data = {
+      api24: {...api24},
+      wharehouse: packages
+    }
 
-    return response(200, packages, connection)
+    return response(200, data, connection)
   } catch (e) {
     console.log(e, 'catch')
     return response(400, e, null)
--- Api/UniversalPackages/universalStorage.js
+++ Api/UniversalPackages/universalStorage.js
@@ -1,16 +1,7 @@
-const read = (params) => {
-  let _limit = 25
-  let _page = 0
-  
-  if (params.page && params.page > 1) {
-    _page = (params.page - 1) * _limit
-  } else {
-    _page = 0
-  }
-  
-  const query = `SELECT * FROM packages_universe A
-                 ${peprareData(params)}
-                 ORDER BY id DESC LIMIT ${_page},${_limit}`
+const read = (tracking) => {
+  const query = `SELECT * FROM paquetes A
+                 WHERE A.tracking = '${tracking.trackingNumber}'
+                 ORDER BY A.package_id`
   console.log(query);
   return query
 }
@@ -21,21 +12,6 @@ const create = (params,date) => {
   return query
 }
 
-const peprareData = (params) => {
-  let where = ''
-  if(params.tracking && params.tracking !== ''){
-    where += where === '' ? `WHERE tracking like '%${params.tracking}%'`:` AND tracking like '%${params.tracking}%'`
-  }
-  
-  if(params.date && params.date !== ''){
-    where += where === '' ? `WHERE created_date = '${params.date}'`:` AND created_date = '${params.date}'`
-  }
-  
-  if(params.carrier && params.carrier !== ''){
-    where += where === '' ? `WHERE carrier_id = ${params.carrier}`:` AND carrier_id = ${params.carrier}`
-  }
-  return where
-}
 module.exports = {
   get: read,
   post: create
