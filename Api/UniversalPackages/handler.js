18b07004c48862b60ec48b4bcff8d5795929097c
userlab-alejandrorodriguez <87735711+userlab-alejandrorodriguez@users.noreply.github.com>
2021-09-28 18:31:10 -0600

    Function create a new ticket in Teamwork Desk custom domain

--- Api/UniversalPackages/handler.js
+++ Api/UniversalPackages/handler.js
@@ -1,18 +1,17 @@
-'use strict'
-const mysql = require('mysql2/promise')
-const moment = require('moment-timezone')
-const request = require('request')
-const isOffline = process.env['IS_OFFLINE']
-const { dbConfig } = require(`${isOffline ? '../..' : '.'}/commons/dbConfig`)
-let { response } = require(`${isOffline ? '../..' : '.'}/commons/utils`)
-let storage = require('./universalStorage')
-const date = moment()
-  .tz('America/Guatemala')
-  .format('YYYY-MM-DD')
+"use strict";
+const mysql = require("mysql2/promise");
+const moment = require("moment-timezone");
+const isOffline = process.env["IS_OFFLINE"];
+const { dbConfig } = require(`${isOffline ? "../.." : "."}/commons/dbConfig`);
+let { response } = require(`${isOffline ? "../.." : "."}/commons/utils`);
+let storage = require("./universalStorage");
+const date = moment().tz("America/Guatemala").format("YYYY-MM-DD");
+const request = require(process.env["IS_OFFLINE"]
+  ? "request"
+  : "../../layers/nodejs/node_modules/request");
 
-const AWS = require('aws-sdk')
-AWS.config.update({ region: 'us-east-1' })
-
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
 
 module.exports.read = async (event, context) => {
   try {
@@ -56,8 +55,46 @@ module.exports.read = async (event, context) => {
 
     return response(200, data, connection)
   } catch (e) {
-    console.log(e, 'catch')
-    return response(400, e, null)
+    console.log(e, "catch");
+    return response(400, e, null);
   }
-}
+};
 
+module.exports.createTicket = async (event, context) => {
+  try {
+
+    let body = JSON.parse(event.body);
+
+    body = {
+      assignedTo: parseInt(process.env.ASSIGNED_TO),
+      inboxId: parseInt(process.env.INBOX_ID),
+      ...body,
+    }
+
+    let option = {
+      method: "post",
+      url: process.env.TEAMWORK_URL,
+      body: JSON.stringify(body),
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.TEAMWORK_TOKEN}`,
+      },
+    };
+
+    const promise = await new Promise((resolve, reject) => {
+      request(option, (error, result, body) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        }
+        console.log(body, result);
+        resolve(body);
+      });
+    });
+
+    return response(200, promise, null);
+
+  } catch (error) {
+    throw error;
+  }
+};
