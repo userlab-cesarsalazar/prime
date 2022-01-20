bfd7261f755e6601b5df630f480ecc682d53d558
userlab-luisdeleon <59143067+userlab-luisdeleon@users.noreply.github.com>
2022-01-20 10:11:20 -0600

    Export manifest
    Fix name on export manifest function (dev & prod)

--- Api/Manifest/manifestStorage.js
+++ Api/Manifest/manifestStorage.js
@@ -34,13 +34,14 @@ const getPackagesByManifestId = params => {
       ? 'AND (A.master <> "" OR A.master IS NOT NULL) AND (A.poliza <> "" OR A.poliza IS NOT NULL) ORDER BY guia ASC'
       : 'ORDER BY guia ASC'
 
-  return `SELECT A.package_id, A.tracking, S.name as supplier_name, C.client_name, A.weight, A.description,
+  return `SELECT A.package_id, A.tracking, S.name as supplier_name, U.name as client_name, A.weight, A.description,
     A.guia as warehouse, round(A.costo_producto ,2) as costo_producto, A.cif, A.tasa, A.status, A.importe, A.guia, A.cif, A.dai,
     A.master, A.poliza, A.manifest_id, A.total_iva, A.total_a_pagar, A.ing_date, A.pieces, A.tariff_code,
     A.voucher_bill, A.voucher_payment, T.description AS tariff_description, T.id AS tariff_code,
     T.code AS tariff_nro_partida, CAST((T.tasa * 100) AS SIGNED) AS tariff_tasa,C.nit AS nit,valor_miami, A.costo_producto as costo_producto_b
     FROM paquetes A
-    LEFT JOIN clientes C on A.client_id = C.client_id 
+    INNER JOIN clientes C on A.client_id = C.client_id
+    INNER JOIN usuarios U on C.id_usuario = U.id 
     LEFT JOIN suppliers S on A.supplier_id = S.id
     LEFT JOIN tariffs T on A.tariff_code = T.id
     WHERE A.manifest_id = ${params.manifest_id} ${polizaWhereCondition} ${noNullWhereCondition}`
